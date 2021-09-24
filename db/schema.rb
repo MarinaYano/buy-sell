f86341bb0b55856afb55dbd4878cdbccc823dfb1
Marina Yano <myano2621@gmail.com>
2021-09-25 02:59:03 +0900

    GSDC前調整

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 2021_09_01_074514) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "profile"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
